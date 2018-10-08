cae95fe90c247ebc214bf09c13f123dedc4ef62f
Neil Seejoor <nvseejoor@gmail.com>
2018-10-08 19:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 class Driver {
   constructor(name, startDate) {
     this.name = name;
